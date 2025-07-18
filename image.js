3fabf04afa6730a8e74ba12bc7f2e4145fb244af
Matt-proj <officialjtsung@gmail.com>
2025-07-18 14:30:50 +0800

    Update image.js

--- image.js
+++ image.js
@@ -1,6 +1,15 @@
 //import constant images eg. Logos
 import GlowNusLogo_ from './images/GlowNusLogo.png'
 import GlowNusBanner_ from './images/GlowNusBanner.png'
+import FoundingTeamGrpPic_ from './images/FoundingTeamGrpPic.png'
+import Founder_ from './images/Founder.png'
+import CoFounder_ from './images/CoFounder.png'
+import FoundingVPInt_ from './images/FoundingVPInt.png'
+import FoundingEventsDirector_ from './images/FoundingEventsDirector.png'
+import FoundingSecretary_ from './images/FoundingSecretary.png'
+import FoundingIndustryOutreach_ from './images/FoundingIndustryOutreach.png'
+import FoundingMarketingDirector_ from './images/FoundingMarketingDirector.png'
+import FoundingFinSec_ from './images/FoundingFinSec.png'
 
 //import images that may or may not change eg. event poster/logo
 
@@ -13,7 +22,15 @@ import GlowNusBanner_ from './images/GlowNusBanner.png'
 //export constant images 
 export const GlowNusLogo = GlowNusLogo_
 export const GlowNusBanner = GlowNusBanner_
-
+export const FoundingTeamGrpPic = FoundingTeamGrpPic_
+export const Founder = Founder_
+export const CoFounder = CoFounder_
+export const FoundingVPInt = FoundingVPInt_
+export const FoundingEventsDirector = FoundingEventsDirector_
+export const FoundingSecretary = FoundingSecretary_
+export const FoundingIndustryOutreach = FoundingIndustryOutreach_
+export const FoundingMarketingDirector = FoundingMarketingDirector_
+export const FoundingFinSec = FoundingFinSec_
 //export images that may or may not change
 
 
