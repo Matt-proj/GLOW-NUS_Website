0f8afae85d7b1657f2d8621573f9673d0cf938bd
Matt-proj <officialjtsung@gmail.com>
2025-07-18 14:02:39 +0800

    Update image.js

--- image.js
+++ image.js
@@ -1,3 +1,21 @@
 //import constant images eg. Logos
 import GlowNusLogo_ from './images/GlowNusLogo.png'
 import GlowNusBanner_ from './images/GlowNusBanner.png'
+
+//import images that may or may not change eg. event poster/logo
+
+//import images that will change every semester
+
+
+//imports end//
+//Exports start//
+
+//export constant images 
+export const GlowNusLogo = GlowNusLogo_
+export const GlowNusBanner = GlowNusBanner_
+
+//export images that may or may not change
+
+
+//export images that will change every semester/
+
